e1e2547601c1f218fe6937c573db23615a5501b6
cw-milesleung <milesleung0724@gmail.com>
2024-04-01 22:32:31 -0600

    first commit

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
-# Start the cron daemon in the background
-cron &
+docker compose up init-airflow
 
-# Execute the Python script
-python /app/elt_script.py
+sleep 5
+
+docker compose up -d
+
+sleep 5
+
+cd airbyte
+
+# Check if docker-compose.yml exists in the current directory
+if [ -f "docker-compose.yaml" ]; then
+  # If it exists, run docker-compose up
+  docker-compose up -d
+else
+  # Otherwise, run the setup script
+  ./run-ab-platform.sh
+fi
